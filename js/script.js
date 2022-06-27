fe8c527456b54e38c05233f78a2be1aa53d3035f
Feliks Buszta <f.buszta@gmail.com>
2022-06-27 09:25:44 +0200

    i created a page to learn how to create a search engine

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const li = document.querySelectorAll('li')
+const search = document.querySelector('.search')
+
+const searchDrink = e => {
+	const text = e.target.value.toLowerCase()
+
+	li.forEach(el => {
+		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = 'block'
+		} else {
+			el.style.display = 'none'
+		}
+	})
+}
+search.addEventListener('keyup', searchDrink)
